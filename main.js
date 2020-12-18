619fb0ebce88ed7264a38aa3886226cc7a61dc10
zena86 <76158579+zena86@users.noreply.github.com>
2020-12-18 12:39:52 +0300

    source

---
+++ main.js
@@ -0,0 +1,51 @@
+jQuery(function() {
+    
+   //Слайдер Richard Parcker
+    
+    $('.slider').slick({
+        //Скорость пролистывания по умолчанию 300ms
+        speed: 1000,
+        //Бесконечность слайдов
+        infinite: false,
+        // Выстраивает основной слайд по центру
+        centerMode: true,
+        centerPadding: '27.3vw',
+    });
+$('.slider').slick('goTo',1);
+    
+    //Слайдер Amazing Features
+    
+$('.fade').slick({
+    infinite: true,
+    speed: 50,
+    fade: true,
+    cssEase: 'linear',
+});
+    //Слайдер Screenshots
+    
+$('.fadeScreenshots').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 50,
+    fade: true,
+    cssEase: 'linear'
+});
+    
+    //Видео
+    
+    $('.sectionWithVideo').click(function () {  
+   var mediaVideo = $("#guysVideo").get(0);    
+   if (mediaVideo.paused) {
+       mediaVideo.play();
+       $(".iconAndTimeVideo").hide();
+   } else {
+       mediaVideo.pause();
+       $(".iconAndTimeVideo").show();
+  }
+});
+    
+}); 
+
+
+
